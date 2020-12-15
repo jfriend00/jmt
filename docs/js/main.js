e3131cc6bf451212a4ec5ea5ea8ceb5a71f55a2d
jfriend00 <jfriend00@users.noreply.github.com>
2020-12-14 22:53:43 -0800

    Add left/right arrow navigation

--- docs/js/main.js
+++ docs/js/main.js
@@ -80,6 +80,29 @@ function configureArrowLinks(currentIndex) {
     }
 }
 
+// handle left/right arrow for moving foward/back in the trip
+function configureArrowKeys() {
+    window.addEventListener('keydown', function(event) {
+        switch (event.keyCode) {
+            case 37: // left arrow
+            {
+                let arrowLinks = document.querySelectorAll("#navArrows a");
+                window.location = arrowLinks[0].href;
+                break;
+            }
+            case 39: // right arrow
+            {
+                let arrowLinks = document.querySelectorAll("#navArrows a");
+                window.location = arrowLinks[1].href;
+                break;
+            }
+            default:
+                break;
+        }
+    });
+}
+
 let currentIndex = getPageIndex();
 configureBottomLinks(currentIndex);
 configureArrowLinks(currentIndex);
+configureArrowKeys();
