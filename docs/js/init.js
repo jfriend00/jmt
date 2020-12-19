6fa076020e2a7e42395f0ec95e850d51038e5bcc
jfriend00 <jfriend00@users.noreply.github.com>
2020-12-18 17:29:27 -0800

    Dynamically adjust content offset to match header height
    For when header wraps and is larger than expected

--- docs/js/init.js
+++ docs/js/init.js
@@ -6,3 +6,24 @@ function captionLoaded(img) {
         caption.style.display = "table-caption";
     }
 }
+
+// dynamically adjust content top margin based on height of fixed heaader in
+// case a narrow width or large fonts cause the title to wrap to multiple lines
+function adjustForHeaderHeight() {
+    let headerHeight = document.getElementById("header").offsetHeight;
+
+    console.log("headerHeight: ", headerHeight);
+    let content = document.getElementById("content");
+    console.log("contentTop before positioning: ", content.getBoundingClientRect(content).top);
+
+    document.getElementById("content").style.marginTop = headerHeight + "px";
+
+    console.log("contentTop after positioning: ", content.getBoundingClientRect(content).top);
+}
+
+function initHeader() {
+    // readjust whenever the window size is changed
+    window.addEventListener("resize", adjustForHeaderHeight);
+    window.addEventListener("orientationchange", adjustForHeaderHeight);
+    adjustForHeaderHeight();
+}
