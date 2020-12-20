9769e7328b4f177589749afe5f4f85a2e53d5328
jfriend00 <jfriend00@users.noreply.github.com>
2020-12-20 08:33:08 -0800

    Add click support to remove popup menu

--- docs/js/main.js
+++ docs/js/main.js
@@ -20,4 +20,27 @@ function configureArrowKeys() {
     });
 }
 
+function configureMenuClick() {
+    // this is complicated because the click event comes after
+    // the focus event so even when the menu was not yet visible,
+    // it will already be visible when the initial click addEventListener
+    // comes through.  So, we have make sure the click was some period of
+    // time after the focus event before we process it
+    const clickTime = 300;
+    const burger = document.querySelector("#menu .burger");
+    let burgerFocusTime = Date.now();
+    burger.addEventListener("click", function(e) {
+        let popup = document.querySelector("#menu .popup");
+        let style = window.getComputedStyle(popup);
+        if (style.display !== "none" && Date.now() - burgerFocusTime > clickTime) {
+            burger.blur();
+            e.preventDefault();
+        }
+    });
+    burger.addEventListener("focus", function(e) {
+        burgerFocusTime = Date.now();
+    });
+}
+
 configureArrowKeys();
+configureMenuClick();
