29d3950b49baa710fee9a93e943f26646f4a3c82
jfriend00 <jfriend00@users.noreply.github.com>
2020-12-20 09:44:01 -0800

    Add mini console for debugging

--- docs/js/main.js
+++ docs/js/main.js
@@ -1,3 +1,19 @@
+// for logging into a window on devices that you can't easily see
+// the console
+function log(...args) {
+    let logger = document.querySelector("#log");
+    if (!logger) {
+        logger = document.createElement("div");
+        logger.style.height = "70vh";
+        logger.style.margin = "0.5rem";
+        logger.id = "log";
+        document.body.appendChild(logger);
+    }
+    let msg = document.createElement("div");
+    msg.textContent = args.join(" ");
+    logger.appendChild(msg);
+}
+
 // handle left/right arrow for moving foward/back in the trip
 function configureArrowKeys() {
     window.addEventListener('keydown', function(event) {
@@ -30,6 +46,7 @@ function configureMenuClick() {
     const burger = document.querySelector("#menu .burger");
     let burgerFocusTime = Date.now();
     burger.addEventListener("click", function(e) {
+        log("click")
         let popup = document.querySelector("#menu .popup");
         let style = window.getComputedStyle(popup);
         if (style.display !== "none" && Date.now() - burgerFocusTime > clickTime) {
@@ -38,8 +55,13 @@ function configureMenuClick() {
         }
     });
     burger.addEventListener("focus", function(e) {
+        log("focus")
         burgerFocusTime = Date.now();
     });
+    burger.addEventListener("blur", function(e) {
+        log("blur")
+    });
+
 }
 
 configureArrowKeys();
